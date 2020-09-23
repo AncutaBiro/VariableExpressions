088a9474fdc7b3812c252c430c2a1064163227bc
AncutaBiro <ancuta.biro@yahoo.com>
2020-09-23 21:41:37 +0300

    Add project files.

---
+++ VariableExpressions/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace VariableExpressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            byte x = 250;
+            byte y = 10;
+            x = (byte)(x + y);
+            Console.WriteLine(x);
+            Console.Read();
+        }
+    }
+}
